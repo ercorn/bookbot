ce0f0327d92a660da5ac7c68a836249209c34dc5
rcorn013 <rctexvn@gmail.com>
2025-01-07 15:41:13 -0500

    added count_chars function to return a dict of characters and # of appearances in text

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+def count_chars(text):
+    chars = {}
+    words = text.lower().split()
+    for word in words:
+        for char in word:
+            if char in chars:
+                chars[char] += 1
+            else:
+                chars[char] = 1
+    return chars
+
 def read_book_from_file(path):
     with open(path) as file:
         return file.read()
@@ -9,7 +20,8 @@ def read_book_from_file(path):
 def main():
     book_path = "books/frankenstein.txt"
     book = read_book_from_file(book_path)
-    print(book)
+    #print(book)
     print(f"There are {count_words(book)} words.")
+    print(f"TEST: {count_chars(book)}")
 
 main()
