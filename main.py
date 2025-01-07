4f9c9290bfa8e56bb1fba4a8f0cc6694f3bf6346
rcorn013 <rctexvn@gmail.com>
2025-01-07 15:09:46 -0500

    added word count to main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def count_words(book):
+    words = book.split()
+    return len(words)
+
 def main():
     with open("books/frankenstein.txt") as file:
         file_contents = file.read()
         print(file_contents)
+        print(f"There are {count_words(file_contents)} words.")
 
 main()
