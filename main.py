1d9d143d91720e4b9ed4637e862b1843e7454741
rcorn013 <rctexvn@gmail.com>
2025-01-07 16:32:34 -0500

    added book report listing # of words total and an ordered list of character counts

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ def count_chars(text):
             chars[lowered] = 1
     return chars
 
+def convert_to_list_dict(dict):
+    dict_list = []
+    for member in dict:
+        if member.isalpha():
+            dict_list.append({"char": member, "count": dict[member]})
+    return dict_list
+
+def sort_on(dict): #function takes a dictionary return the value of the "count" key to sort with the .sort() method
+    return dict["count"]
+
 def read_book_from_file(path):
     with open(path) as file:
         return file.read()
@@ -19,8 +29,15 @@ def read_book_from_file(path):
 def main():
     book_path = "books/frankenstein.txt"
     book = read_book_from_file(book_path)
-    #print(book)
-    print(f"There are {count_words(book)} words.")
-    print(f"CHARS: {count_chars(book)}")
+
+    chars_counts_list = convert_to_list_dict(count_chars(book))
+    chars_counts_list.sort(reverse=True, key=sort_on)
+
+    print(f"[--- Begin report of {book_path} ---]")
+    print(f"There are {count_words(book)} words in this text.")
+    print()
+    for dict in chars_counts_list:
+        print(f"The '{dict["char"]} character was found {dict["count"]} times")
+    print("[--- End report ---]")
 
 main()
